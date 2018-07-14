6bdf17e303510e473c6594fce33e1b86bc8a3b0f
simov <simeonvelichkov@gmail.com>
2018-07-14 19:44:54 +0300

    Add items and detail methods

--- index.js
+++ index.js
@@ -1,16 +1,46 @@
 
 var request = require('request-compose').client
 
-var extract = require('./lib/extract')
-var format = require('./lib/format')
+var item = require('./lib/item')
+var detail = require('./lib/detail')
+
+var config = require('./config/webstore')
 
 
 module.exports = {
 
-  extension: ({url, id}) =>
+  items: ({search, category, rating, features, count, offset, version}) =>
     request({
-      url: url || `https://chrome.google.com/webstore/detail/${id}`
+      method: 'POST',
+      url: 'https://chrome.google.com/webstore/ajax/item',
+      qs: {
+        searchTerm: search,
+        category,
+        features: (features || []).map((key) => config.features[key])
+          .concat(config.rating[rating]).filter(Boolean),
+        count: count || 5,
+        token: offset ? `${offset}@${offset}` : undefined,
+        pv: version || config.version,
+      },
     })
-    .then(({body}) => format(extract(id, body)))
+    .then(({body}) =>
+      JSON.parse(body.slice(5))[1][1].map((i) => item.transform(item.extract(i))))
+  ,
+
+  detail: ({id, related, more, version}) =>
+    request({
+      method: 'POST',
+      url: 'https://chrome.google.com/webstore/ajax/detail',
+      qs: {
+        id,
+        pv: version || config.version,
+      },
+    })
+    .then(({body}) => ((json = JSON.parse(body.slice(5))) => Object.assign(
+      detail.transform(detail.extract(json[1][1])),
+      related && {related: json[1][2].map((i) => item.transform(item.extract(i)))},
+      more && {more: json[1][3].map((i) => item.transform(item.extract(i)))},
+    ))())
+  ,
 
 }
