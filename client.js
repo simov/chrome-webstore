33c902f9fffcf65779eab933c51bc648f0f5c8dc
simov <simeonvelichkov@gmail.com>
2020-11-01 22:48:03 +0200

    Bump api version

--- client.js
+++ client.js
@@ -6,7 +6,7 @@ var item = require('./map/item')
 var review = require('./map/review')
 var issue = require('./map/issue')
 
-var VERSION = '20201002'
+var VERSION = '20201016'
 
 
 module.exports = {
